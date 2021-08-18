f2a484fbddb27435dcdc3cd5c106112bd3654186
Bicet <bicets@gmail.com>
2021-08-18 20:42:41 +0800

    instantnoodlep: Revert to stock fingerprint
    Breaks AR
    Thanks to bicet for pointing this out
    
    Also update product flags from Lineage

--- ssos_instantnoodlep.mk
+++ ssos_instantnoodlep.mk
@@ -44,7 +44,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlus8Pro \
-    PRODUCT_NAME=OnePlus8Pro \
-    PRIVATE_BUILD_DESC="OnePlus8Pro-user 10 QKQ1.191222.002 2005301332 release-keys"
+    PRODUCT_NAME=OnePlus8Pro_EEA \
+    PRIVATE_BUILD_DESC="OnePlus8Pro-user 11   release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
+BUILD_FINGERPRINT := OnePlus/OnePlus8Pro/OnePlus8Pro:10/QKQ1.191222.002/2005301332:user/release-keys
